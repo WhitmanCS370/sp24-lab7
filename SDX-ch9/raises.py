9c873680191fb84beeb207c3899756344f0d70b5
Steven <steven.lin@leadvisor.net>
2024-02-27 13:56:24 -0800

    initial

---
+++ SDX-ch9/raises.py
@@ -0,0 +1,29 @@
+class Fake:
+    def __init__(self, func=None, value=None):
+        self.calls = []
+        self.func = func
+        self.value = value
+
+    def __call__(self, *args, **kwargs):
+        self.calls.append([args, kwargs])
+        if self.func is not None:
+            return self.func(*args, **kwargs)
+        return self.value
+
+
+class ContextFake(Fake):
+    def __init__(self, name, func=None, value=None):
+        super().__init__(func, value)
+        self.name = name
+        self.original = None
+
+    def __enter__(self):
+        assert self.name in globals()
+        self.original = globals()[self.name]
+        globals()[self.name] = self
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        globals()[self.name] = self.original
+
+# Solution: add argument to ContextFake to check for expected exception, and raise it if not found
