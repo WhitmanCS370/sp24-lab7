c67e4d096c160cc4dd9c34fe4a2446e788178a7b
AidanVB <vonbucha@whitman.edu>
2024-02-27 13:29:12 -0800

    Solved Logging to a File from 9.6

--- SDX-ch8/wrap_infinite.py
+++ SDX-ch8/wrap_infinite.py
@@ -6,5 +6,15 @@ def logging(value):
     original(value)
     print("after call")
 
-original = logging
-original("example")
+def logFile(value, file):
+    print("before call")
+    with open(file, "a") as f:
+        f.write(f"{value}\n")
+    # original(value, file)
+    print("after call")
+    # close file
+    f.close()
+
+
+original = logFile
+original("example", "logFile.txt")
