2b868cc408cf68a7e71cbf71ff5377c05b9bca78
didwayg <75285447+didwayg@users.noreply.github.com>
2024-02-27 13:55:47 -0800

    Excersises

--- SDX-ch8/mock_context.py
+++ SDX-ch8/mock_context.py
@@ -1,5 +1,29 @@
 from mock_object import Fake
+import pytest
 from util import run_tests
+
+def WrongExepctionError(Exception):
+    pass
+
+# [contextraises]
+class Raises(Fake):
+    def __init__(self, expected_exception):
+        self.expected_exception = expected_exception
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if exc_type == None:
+            raise AssertionError(f"{self.expected_exception.__name__} was not raised")
+        if not issubclass(exc_type, self.expected_exception):
+            raise AssertionError("Expected exception %s but got %s" % (self.expected_exception, exc_type))
+
+        return True
+
+
+
+
 
 # [contextfake]
 class ContextFake(Fake):
@@ -29,6 +53,17 @@ def check_no_lasting_effects():
         assert len(fake.calls) == 1
     assert subber(2, 3) == -1
 # [/test]
+
+def test_raises():
+    with Raises(ZeroDivisionError):
+        1 / 0
+
+    with Raises(ValueError):
+        raise ValueError("foo")
+
+    with pytest.raises(AssertionError):
+        with Raises(ZeroDivisionError):
+            pass
 
 if __name__ == "__main__":
     run_tests(globals(), "check_")
