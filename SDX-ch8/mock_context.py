2760d09c55e148b5c2a149f72985bf301f59ebc9
Andrew Tate <98910049+tater-tot25@users.noreply.github.com>
2024-02-27 13:54:12 -0800

    As Far as We could Get

--- SDX-ch8/mock_context.py
+++ SDX-ch8/mock_context.py
@@ -1,5 +1,6 @@
 from mock_object import Fake
 from util import run_tests
+import time
 
 # [contextfake]
 class ContextFake(Fake):
@@ -16,7 +17,35 @@ class ContextFake(Fake):
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
         globals()[self.name] = self.original
+        
 # [/contextfake]
+def raises(func, exceptionToRaise, **kwargs):
+    try:
+        func(**kwargs)
+    except exceptionToRaise:
+        return
+    raise Exception("Function Did Not Fail")
+    return
+
+class Timer():
+    def __init__(self):
+        self.timeStarted = 0
+        self.timeElapsed = 0
+    
+    def __enter__(self):
+        self.timeStarted = time.time()
+    
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        pass
+        
+    def getTimeElapsed(self):
+        self.timeElapsed = time.time() - self.timeStarted
+        return self.timeElapsed
+    
+def testFunction():
+    with Timer() as Start:
+        time.sleep(5)
+        print(Start.getTimeElapsed())
 
 # [test]
 def subber(a, b):
